fe19b94046c7ff03368216e26910c099daf1e60d
MinThuKhant2 <minthukhantminthukhant78@gamil.com>
2023-05-21 08:43:19 +0630

    21/5/23

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const fs = require('fs')
-const node = fs.readFileSync('node.txt')
-console.log(node.toString())
+// const fs = require('fs')
+// const node = fs.readFileSync('node.txt')
+// console.log(node.toString())
+
+const http = require('http')
+http.createServer((req,res) =>{
+    res.end('MinThuKahnt')
+}).listen(3000,() =>{
+    console.log('server runing on port 3300')
+})
